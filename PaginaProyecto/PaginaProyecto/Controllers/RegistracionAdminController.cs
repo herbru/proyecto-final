abbdef9c155b62d074f2ee768143e666856a6819
Hernan Brunstein <hernanb2000@gmail.com>
2017-05-08 22:22:49 -0300

    cree el modelo de la DB

---
+++ PaginaProyecto/PaginaProyecto/Controllers/RegistracionAdminController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PaginaProyecto.Models;
+
+namespace PaginaProyecto.Controllers
+{
+    public class RegistracionAdminController : Controller
+    {
+        // GET: RegistracionAdmin/RegistrarAdmin
+        public ActionResult RegistrarAdmin()
+        {
+            return View();
+        }
+
+        // POST: RegistracionAdmin/RegistrarAdmin
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult RegistrarAdmin(Administrador oAdmin)
+        {
+            if (ModelState.IsValid)
+            {
+
+                return RedirectToAction("");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        // GET: RegistracionAdmin/LoguearAdmin
+        public ActionResult LoguearAdmin()
+        {
+            return View();
+        }
+
+        // POST: RegistracionAdmin/LoguearAdmin
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult LoguearAdmin(Administrador oAdmin)
+        {
+            if (ModelState.IsValid)
+            {
+                dbProyecto.Create(oAdmin);
+                return RedirectToAction("HomeAdmin");
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ PaginaProyecto/PaginaProyecto/Models/Administrador.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace PaginaProyecto.Models
+{
+    public class Administrador
+    {
+        [Key]
+        public int AdminID { get; set; }
+
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [StringLength(60, ErrorMessage = "El nombre debe tener menos de 60 caracteres")]
+        [Display(Name = "Nombre")]
+        public string Nombre { get; set; }
+
+        [Display(Name = "URL de Imagen")]
+        [DataType(DataType.ImageUrl, ErrorMessage = "Debe ingresar la url de una imagen en el campo {0}")]
+        public HttpPostedFileBase Imagen { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [Display(Name = "Email")]
+        [EmailAddress(ErrorMessage = "Direccion de Email invalida")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [Display(Name = "Contraseña")]
+        [StringLength(40, MinimumLength = 8, ErrorMessage = "La contraseña debe tener como minimo 8 caracteres")]
+        [DataType(DataType.Password, ErrorMessage = "Debe ingresar una contraseña")]
+        public string Contraseña { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmar contraseña")]
+        [Compare("Contraseña", ErrorMessage = "La contraseña y la confirmacion no son iguales")]
+        public string ConfirmarContraseña { get; set; }
+    }
+}
